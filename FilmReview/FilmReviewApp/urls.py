15550eb3ba1a8a5dc52c59346a468fcba4ef9b30
see-quad-sir <hp.personalcomputer.mine@gmail.com>
2024-07-30 12:39:32 +0900

    fix several UI elements

--- FilmReview/FilmReviewApp/urls.py
+++ FilmReview/FilmReviewApp/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path("user/", views.user, name="user"),
     path("logout/", views.logout, name="logout"),
     path("movie/<int:movie_id>/<int:user_id>", views.movie, name="movie"),
-    path("review/<int:movie_id>/", views.review, name="review"),
 ]
--- FilmReview/FilmReviewApp/views.py
+++ FilmReview/FilmReviewApp/views.py
@@ -23,12 +23,14 @@ def user(request):
             context = {"movie_list": movie_list, "user": user}
             return render(request, "FilmReviewApp/main.html", context)
         elif "signup" in request.POST:
+            if User.objects.filter(user_name=request.POST["username"]).exists():
+                return HttpResponseRedirect(reverse("FilmReviewApp:index"))
             user_name = request.POST["username"]
             password = request.POST["password"]
             user = User(user_name=user_name, password=password)
             user.save()
             context = {"movie_list": movie_list, "user": user}
-            render(request, "FilmReviewApp/main.html", context)
+            return render(request, "FilmReviewApp/main.html", context)
     return HttpResponseRedirect(reverse("FilmReviewApp:index"))
 
 
@@ -61,26 +63,3 @@ def movie(request, movie_id, user_id):
 
     context = {"movie": movie, "reviews": reviews, "user_id": user_id, "form": form}
     return render(request, "FilmReviewApp/movie.html", context)
-
-
-def review(request, movie_id):
-    if request.method == "POST":
-        movie = Movie.objects.get(pk=movie_id)
-        user = User.objects.get(pk=request.POST["user_id"])
-        review = request.POST["review"]
-        star_rating = request.POST["star_rating"]
-        review = Review(
-            movie=movie,
-            user=user,
-            review=review,
-            star_rating=star_rating,
-        )
-        num_reviews = Review.objects.filter(movie_id=movie_id).count()
-        rating_diff = star_rating - movie.star_rating_avg
-        movie.star_rating_avg += rating_diff / (num_reviews + 1)
-        review.save()
-        movie.save()
-        return HttpResponseRedirect(
-            reverse("FilmReviewApp:movie", args=(movie_id, user.id))
-        )
-    return HttpResponseRedirect(reverse("FilmReviewApp:index"))
